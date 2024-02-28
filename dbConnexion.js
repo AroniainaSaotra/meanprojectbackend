3c13c96c1c4d3106c11b0f12d03cd173614a19cc
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2024-02-29 00:28:00 +0300

    ii

--- dbConnexion.js
+++ dbConnexion.js
@@ -3,7 +3,7 @@ require('dotenv/config');
 mongoose.set('strictQuery',true);
 console.log(process.env.DB_URL)
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect("mongodb+srv://Meanm1db:MirantsoaSaotra@cluster0.lb6lr6y.mongodb.net/MEAN_BeautySalon", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
